66f8966834b7fc8dcfa8e633cf5c3474212bb6e0
KiriZhu <kiribaty88@gmail.com>
2020-08-30 21:58:51 +0200

    refactored Messages to func comp and fixed bug with scrolltobottom caused by ref bug

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,34 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import moment from 'moment';
 import './styles/Messages.css';
 
-class Messages extends React.Component {
-  componentDidUpdate() {
-    // scroll to bottom
-    window.scrollTo(0, this.refs.chat.scrollHeight);
-  }
+function Messages({ messages }) {
+  const ulRef = useRef(null);
 
-  render() {
-    const { messages } = this.props;
+  useEffect(() => {
+    window.scrollTo(0, ulRef.current.scrollHeight);
+  }, [messages]);
 
-    const chatMessages = messages.map((chat, key) => (
-      <li className='messages-li' key={key}>
-        <p className='messages-timestampText'>
-          {moment(chat.timestamp).format('D.M.YYYY HH:mm:ss')}
-        </p>
+  const chatMessages = messages.map((chat, key) => (
+    <li className='messages-li' key={key}>
+      <p className='messages-timestampText'>
+        {moment(chat.timestamp).format('D.M.YYYY HH:mm:ss')}
+      </p>
 
-        <p className='messages-messageText'>
-          {chat.sender}: {chat.message}
-        </p>
-      </li>
-    ));
+      <p className='messages-messageText'>
+        {chat.sender}: {chat.message}
+      </p>
+    </li>
+  ));
 
-    return (
-      <ul className='messages-ul' ref='chat'>
-        {chatMessages}
-      </ul>
-    );
-  }
+  return (
+    <ul className='messages-ul' ref={ulRef}>
+      {chatMessages}
+    </ul>
+  );
 }
 
 export default Messages;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import './styles/UserList.css';
 
-class UserList extends React.Component {
-  render() {
-    const { userlist } = this.props;
+function UserList({ userlist }) {
+  const users = userlist.map((user, key) => (
+    <li className='userList-li' key={key}>
+      <p className='userList-online'>.</p>
 
-    const users = userlist.map((user, key) => (
-      <li className='userList-li' key={key}>
-        <p className='userList-online'>.</p>
+      {user.username}
+    </li>
+  ));
 
-        {user.username}
-      </li>
-    ));
+  return (
+    <div className='userList-container'>
+      <p className='userList-title'>Users in chat</p>
 
-    return (
-      <div className='userList-container'>
-        <p className='userList-title'>Users in chat</p>
-
-        <ul className='userList-ul'>{users}</ul>
-      </div>
-    );
-  }
+      <ul className='userList-ul'>{users}</ul>
+    </div>
+  );
 }
 
 export default UserList;
